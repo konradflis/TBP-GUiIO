5e83ff0d02e36dc91bfc99b56363a9be1ef73aa5
Jakub Tomczyk <jakub.tom16@gmail.com>
2025-03-31 17:47:55 +0200

    parameters tests

--- parameter_test/param_test.py
+++ parameter_test/param_test.py
@@ -1,32 +1,41 @@
 import pandas as pd
 from sources.genetic_alg import GeneticAlgorithm
+from sources.data_structures import MandatorySettingsGEN, OptionalSettingsGEN
+from sources.common_elements import PropagatedElement, ModelProperties
 from pathlib import Path
 import json
 
 # Parametry algorytmu
-population_size = 120
+population_size = 100
 max_generations = 100
 mutation_rate = 0.01
 crossover_rate = 0.7
 tournament_size = 5
 
 # Ścieżka do pliku CSV
-csv_filename = Path(__file__).parent / 'data' / 'test2' / 'result.csv'
+csv_filename = Path(__file__).parent / 'data' / 'test7' / 'result.csv'
 csv_filename.parent.mkdir(parents=True, exist_ok=True)
 
 # Uruchomienie algorytmu 10 razy i zapisanie wyników
+mandatory= MandatorySettingsGEN(
+    population_size=population_size,
+    number_of_measurements = tournament_size,
+    max_generations = max_generations,
+    mutation_rate=mutation_rate,
+    crossover_rate=crossover_rate,
+)
+optional = OptionalSettingsGEN()
 results = []
 for i in range(10):
     ga = GeneticAlgorithm(
-        population_size=population_size,
-        max_generations=max_generations,
-        mutation_rate=mutation_rate,
-        crossover_rate=crossover_rate,
-        tournament_size=tournament_size
+        mandatory = mandatory,
+        optional=optional
     )
     ga.run()
     best_individual = min(ga.population.individuals, key=lambda ind: ind.score)
-    results.append([i + 1, best_individual.score, best_individual.state])
+    best_state = [float(val) for val in best_individual.state]  
+
+    results.append([i + 1, best_individual.score, best_state])
 
 # Zapisanie wyników do DataFrame i potem do pliku CSV
 df = pd.DataFrame(results, columns=["Run", "Best Score", "Best State"])
@@ -35,7 +44,7 @@ df = pd.DataFrame(results, columns=["Run", "Best Score", "Best State"])
 df.to_csv(csv_filename, index=False)
 print(f"Wynik zapisany {csv_filename}")
 
-json_filename = Path(__file__).parent / 'data' / 'test2' / 'params.json'
+json_filename = Path(__file__).parent / 'data' / 'test7' / 'params.json'
 json_filename.parent.mkdir(parents=True, exist_ok=True)
 
 params = {
