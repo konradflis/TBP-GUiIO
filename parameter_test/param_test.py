2eefda3a8332c0e7727026ea5d15da07a0cc5253
Jakub Tomczyk <jakub.tom16@gmail.com>
2025-03-31 17:54:52 +0200

    Update param_test.py

--- parameter_test/param_test.py
+++ parameter_test/param_test.py
@@ -6,20 +6,20 @@ from pathlib import Path
 import json
 
 # Parametry algorytmu
-population_size = 100
-max_generations = 100
+population_size = 500
+max_generations = 10
 mutation_rate = 0.01
 crossover_rate = 0.7
-tournament_size = 5
+number_of_measurements  = 5
 
 # Ścieżka do pliku CSV
-csv_filename = Path(__file__).parent / 'data' / 'test7' / 'result.csv'
+csv_filename = Path(__file__).parent / 'data' / 'test8' / 'result.csv'
 csv_filename.parent.mkdir(parents=True, exist_ok=True)
 
 # Uruchomienie algorytmu 10 razy i zapisanie wyników
 mandatory= MandatorySettingsGEN(
     population_size=population_size,
-    number_of_measurements = tournament_size,
+    number_of_measurements = number_of_measurements ,
     max_generations = max_generations,
     mutation_rate=mutation_rate,
     crossover_rate=crossover_rate,
@@ -33,7 +33,7 @@ for i in range(10):
     )
     ga.run()
     best_individual = min(ga.population.individuals, key=lambda ind: ind.score)
-    best_state = [float(val) for val in best_individual.state]  
+    best_state = [float(val) for val in best_individual.state]
 
     results.append([i + 1, best_individual.score, best_state])
 
@@ -44,7 +44,7 @@ df = pd.DataFrame(results, columns=["Run", "Best Score", "Best State"])
 df.to_csv(csv_filename, index=False)
 print(f"Wynik zapisany {csv_filename}")
 
-json_filename = Path(__file__).parent / 'data' / 'test7' / 'params.json'
+json_filename = Path(__file__).parent / 'data' / 'test8' / 'params.json'
 json_filename.parent.mkdir(parents=True, exist_ok=True)
 
 params = {
